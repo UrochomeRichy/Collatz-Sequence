63030c2ad3e3e9aae72b80034a0ce548193c449c
Urochome <133230428+UrochomeRichy@users.noreply.github.com>
2025-08-15 00:12:02 +0300

    Commiting the collatz sequence challenge

---
+++ Collatz_sequence.py
@@ -0,0 +1,13 @@
+def collatz(number):
+
+        if number % 2 == 0:
+            return number // 2
+        else:
+            return 3 * number + 1
+        
+print("Please enter any integer number of your choice.")        
+while True:
+    break_number = collatz(int(input()))
+    print(break_number)
+    if break_number == 1:
+         break
