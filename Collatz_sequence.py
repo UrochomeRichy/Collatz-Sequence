53af3835678f28b6d604d6f250eb750ac8f9661b
Urochome <133230428+UrochomeRichy@users.noreply.github.com>
2025-08-15 10:29:49 +0300

    My collatz sequence challenge

--- Collatz_sequence.py
+++ Collatz_sequence.py
@@ -4,10 +4,14 @@ def collatz(number):
             return number // 2
         else:
             return 3 * number + 1
-        
-print("Please enter any integer number of your choice.")        
+
+print("Please enter any integer number of your choice.")
+
 while True:
-    break_number = collatz(int(input()))
-    print(break_number)
-    if break_number == 1:
-         break
+    try:
+        break_number = collatz(int(input()))
+        print(break_number)
+        if break_number == 1:
+            break
+    except ValueError:
+        print("That's not a valid integer. Please try again.")
